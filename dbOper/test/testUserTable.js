a28db06ee579917bc42fea7267d00e72f86f2fb3
plxmbh <mengmengqiaoqiao@gmail.com>
2016-01-27 20:21:42 +0900

    テストコード

--- dbOper/test/testUserTable.js
+++ dbOper/test/testUserTable.js
@@ -1,4 +1,4 @@
-var User = require('../js/tableUser.js');
+var User = require('../js/tableUser');
 
 var tbUser = new User();
 tbUser.insertUser('{"account":"limh","password":"mengqiao","name":"liminhui","email":"mengmengqiaoqiao@gmail.com"}');
